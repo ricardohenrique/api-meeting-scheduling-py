01823323a6434a387e0b838711270c0afcc88381
Ricardo Mota <ricardo.mota@ftd.com.br>
2019-02-15 16:24:45 -0200

    created an app

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,4 +1,4 @@
-"""api_meeting_scheduling_py URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    path('polls/', include('polls.urls')),
 ]
